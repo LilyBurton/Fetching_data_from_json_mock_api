248ac5bd0aeb4e8f45683a3e9904546968c019d7
LilyBurton <lil@192.168.1.130>
2024-07-08 16:54:02 +0100

    Learning about fetching data api from json placeholder

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
+import Form from './Form';
+import List from './List';
 
 function App() {
+  const API_URL = 'https://jsonplaceholder.typicode.com/';
+  const [type, setType] = useState('users');
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      try {
+        const response = await fetch(`${API_URL}${type}`);
+        const data = await response.json()
+        setItems(data)
+      } catch (err) {
+        console.log(err)
+      }
+    }
+
+    fetchItems();
+
+  }, [type])
+ 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Form type={type} setType={setType} />
+      <List items={items} />
+     
     </div>
   );
 }
